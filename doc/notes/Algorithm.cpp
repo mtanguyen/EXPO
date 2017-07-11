3cb7a577c2b8bee3f5f1c303093b10481222f0a3
My Nguyen <mynguyen@Mys-MacBook-Air.local>
2017-07-11 13:03:04 -0400

    New changes

---
+++ doc/notes/Algorithm.cpp
@@ -0,0 +1,24 @@
+/* algorithms of FASTNet
+
+Problem: Given a document full of unstructured text, identify key words.
+
+Inputs: A vector [vector<words> parsed] of parsed words without blank spaces, ASCII, symbols, etc. (must contain at least one word)
+
+Output: A string example of a structured sentence [string structured] will be the
+
+
+
+---- Breakdown of Tasks -----
+Alex ==> 1. Read: read in document of text and algorithm to recognize sentence
+         2. Split: split sentence into different words into classes
+
+Adam ==> 3. Creating dictionary plugin
+
+My ==>   4. Sorting algorithm: sort string of classes of words and use plugin of dictionary to categorize word (class, type)
+
+My ==>   5. Syntax: look at type and position
+
+break sentence into words --> class
+categorize each word
+
+*/

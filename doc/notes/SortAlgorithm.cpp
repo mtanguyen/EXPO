cbb0b6a3752b2bf401017f7ad5d6270c3d397002
My Nguyen <mynguyen@mail.usf.edu>
2017-07-17 13:56:58 -0400

    Changes

--- doc/notes/SortAlgorithm.cpp
+++ doc/notes/SortAlgorithm.cpp
@@ -13,7 +13,7 @@ public:
         int position = p;
     }
     void setLineNumber(int num){
-        int LineNumber = num;
+        int lineNumber = num;
     }
     void getClassType(string type){
         string classType = type;
@@ -22,22 +22,22 @@ public:
 
 void sorting_algorithm(string word, int sentence_position, int line_number){
     Word newWord;
-    newWord.setName("MOTOROLA MOBILITY HOLDINGS, INC.,");
-    newWord.setPosition(1);
-    newWord.setLineNumber(2);
-    neword.getClassType = plugin(newWord);
+    newWord.setName(word);
+    newWord.setPosition(sentence_position);
+    newWord.setLineNumber(line_number);
+    //neword.getClassType = plugin(newWord);
 
-    // warning error
-    if (!flag){
-        // type, name of word, what it did, where
-        cout << "[Error]: sorting algorithm failed in code block 53-72." << endl;
-
-    }
-
-    // error -- just one portion
-    if (position = " "){
-
-
-    }
+    // // warning error
+    // if (!flag){
+    //     // type, name of word, what it did, where
+    //     cout << "[Error]: sorting algorithm failed in code block 53-72." << endl;
+    //
+    // }
+    //
+    // // error -- just one portion
+    // if (position = " "){
+    //
+    //
+    // }
 
 }
